c9d2681358c882619bca46e2eca886a4c2b5083c
stadio18 <stadio18@hotmail.com>
2018-03-18 18:48:39 +0200

    Added View all draws And frequency display

--- opapProject/Controllers/MainMenuController.cs
+++ opapProject/Controllers/MainMenuController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using opapProject.Models.Services;
 
 namespace opapProject.Controllers
 {
     public class MainMenuController : Controller
     {
+        private IDrawRepository _repository;
+
+        public MainMenuController(IDrawRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var listOfDraws = _repository.Draws.OrderByDescending(x=>x.DrawNumber).ToList();
+            return View(listOfDraws);
+        }
+
+        public IActionResult DisplayFrequency()
+        {
+            var listOfDraws = _repository.Draws.ToList();
+            return View(listOfDraws);
+            
         }
     }
 }
